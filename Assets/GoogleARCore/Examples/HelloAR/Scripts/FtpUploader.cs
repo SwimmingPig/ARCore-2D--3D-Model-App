601b92db252626d74bcb809f2fe996a89a82d4bc
Andy Chu <aandychutwa@gmail.com>
2018-09-18 02:59:36 +0800

    Version 3.1
    Vesion 3.0 with unused code deleted

--- Assets/GoogleARCore/Examples/HelloAR/Scripts/FtpUploader.cs
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/FtpUploader.cs
@@ -19,15 +19,6 @@ public class FtpUploader : MonoBehaviour
         FilePath = Application.persistentDataPath + "/Input.png";
         //Debug.Log("Path: " + FilePath);
 
-
-        //WebClient client = new System.Net.WebClient();
-        //Uri uri = new Uri(FTPHost + new FileInfo(FilePath).Name);
-
-
-        //client.UploadProgressChanged += new UploadProgressChangedEventHandler(OnFileUploadProgressChanged);
-        //client.UploadFileCompleted += new UploadFileCompletedEventHandler(OnFileUploadCompleted);
-        //client.Credentials = new System.Net.NetworkCredential(FTPUserName, FTPPassword);
-        //client.UploadFileAsync(uri, WebRequestMethods.Ftp.UploadFile, FilePath);
         // Get the object used to communicate with the server.
         FtpWebRequest request = (FtpWebRequest)WebRequest.Create(FTPHost + new FileInfo(FilePath).Name);
         request.Method = WebRequestMethods.Ftp.UploadFile;
@@ -37,11 +28,10 @@ public class FtpUploader : MonoBehaviour
 
         // Copy the contents of the file to the request stream.
         //byte[] fileContents;
-        using (StreamReader sourceStream = new StreamReader(FilePath))
-        {
-            //fileContents = System.Text.Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
-
-        }
+        //using (StreamReader sourceStream = new StreamReader(FilePath))
+        //{
+        //    fileContents = System.Text.Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+        //}
 
         request.ContentLength = fileContents.Length;
 
@@ -49,25 +39,5 @@ public class FtpUploader : MonoBehaviour
         {
             requestStream.Write(fileContents, 0, fileContents.Length);
         }
-
-        //using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
-        //{
-        //    Console.WriteLine($"Upload File Complete, status {response.StatusDescription}");
-        //}
     }
-
-    //void OnFileUploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
-    //{
-    //    Debug.Log("Uploading Progress: " + e.ProgressPercentage);
-    //}
-
-    //void OnFileUploadCompleted(object sender, UploadFileCompletedEventArgs e)
-    //{
-    //    Debug.Log("File Uploaded");
-    //}
-
-    //void Start()
-    //{
-    //    UploadFile();
-    //}
 }
